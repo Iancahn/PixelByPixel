707e09d958b452392fd02bc3261f1391eb551466
Ian Cahn <iancahn@live.com>
2022-08-11 02:30:24 +0200

    Hero Button
    Added button and section

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+//import Images
+
+
 
 function HomeHero() {
     return (
-        <div>
-            <h2>Test</h2>
+        <div className="homehero">
+            <div className="row">
+                <div className="column">
+                    <h1 className="home-h1">Every great businessneeds a great website.</h1>
+                    <p>Welcome to PBP, a webdesign company dedicated to making the website design process easy.</p>
+                    <button className="herobutton">Contact Us</button>
+                </div>
+                <div className="column">
+                    <img src="#" alt="website design in South Africa" />
+                </div>
+            </div>
         </div>
     )
 }
