7759657921ed3c872d699afaad0a01630d712d27
Ian Cahn <iancahn@live.com>
2022-08-21 01:42:59 +0200

    Footer Component almost done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,31 @@ import React from 'react';
 // import components
 
 // import images
-
+import footerLogo from '../img/pixelbypixel-footer-logo.png';
 
 function Footer() {
     return (
         <footer>
             <div className="container">
-                <div>This is footer</div>
+                <div className='row'>
+                    <div className="column-three">
+                        <div>
+                            <img src={footerLogo} alt="pixelbypixel footer logo" id="footer-logo" />
+                            <p>Copyright 2022 | All Rights Reserved</p>
+                        </div>
+                    </div>
+                    <div className="column-three">
+                        <div>
+                            <img src={footerLogo} alt="pixelbypixel footer logo" id="footer-logo" />
+                            <p>This is my footer</p>
+                        </div>
+                    </div>
+                    <div className="column-three">
+                        <div>
+                            <button>Back to Top</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </footer>
     )
