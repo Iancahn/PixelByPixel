151f1ebf9078223cc572e90f204a79ea2aedc5c8
Ian Cahn <iancahn@live.com>
2022-08-21 00:08:16 +0200

    HomeWork component done

--- src/components/HomeAbout.jsx
+++ src/components/HomeAbout.jsx
@@ -11,13 +11,19 @@ function HomeAbout() {
         <section className="home-about">
             <div className="container">
                 <h2>About Us:</h2>
-                <div className="service-grid">
-                    <article className="grid-article">Test 1</article>
-                    <article className="grid-article">Test 1</article>
-                    <article className="grid-article">Test 1</article>
+                <div className="row">
+                    <div className="column">
+                        <p>Since we first started designing websites way back in early 2014,
+                            we understood that a companies website represents much more than just some online banner.
+                            It's the very first interaction potential customers has with your company.</p>
+                    </div>
+                    <div className="column">
+                        <p>Your website has the potential to set you apart from your competitors, both in the design and search engine ranking aspect.
+                            It's for this reason alone that we  when mThe Schwab Foundation has acknowledged social innovators as a new kind of leader – values-driven, inclusive, entrepreneurial individuals who develop sustainable new models across businesses, social development and environmental initiatives.</p>
+                    </div>
                 </div>
             </div>
-        </section>
+        </section >
     )
 }
 
--- src/components/HomeWork.jsx
+++ src/components/HomeWork.jsx
@@ -4,32 +4,59 @@ import React from 'react';
 // import components
 
 // import images
+import nty from '../img/nty-website-design.jpg';
+import jaco from '../img/jaco-terblanche-website-design.jpg';
 import greenco from '../img/greenco-website-development.jpg';
+import huweliksfokus from '../img/huweliksfokus-website-development.jpg';
+import galaxydoors from '../img/galaxy-doors-website-development.jpg';
+import cycad from '../img/cycad-estate-websitedesign.jpg';
 
 function HomeServices() {
     return (
         <section className="home-work">
 
 
-            <div className="container white-font">
+            <div className="container2 white-font">
                 <div className="portfolio-work">
-                    <div className="portfolio-column">
-                        <h2>Some of the website design projects we just recently launched</h2>
-                        <p>Here's a small selection of the most recent projects we've launched. For a broader sense of our work, check out our other website design projects.</p>
+                    <div className="portfolio-column sticky">
+                        <div className="sticky">
+                            <h2>Some of the website design projects we just recently launched</h2>
+                            <p>Here's a small selection of the most recent projects we've launched. For a broader sense of our work, check out our other website design projects.</p>
+                        </div>
                     </div>
                     <div className="portfolio-column">
-                        <h2>Website designs our company just launched</h2>
-                        <h2>Website designs our company just launched</h2>
-                        <h2>Website designs our company just launched</h2>
-                        <h2>Website designs our company just launched</h2>
-                        <h2>Website designs our company just launched</h2>
+                        <div className="portfolio-column-div1 rem-spacing">
+                            <img src={nty} alt="Some of our latest website design projects" />
+                            <h3>NTY Steel</h3>
+                            <p className="small-project-text">Website Design | Wordpress</p>
+                        </div>
+                        <div className="portfolio-column-div1">
+                            <img src={jaco} alt="Some of our latest website design projects" />
+                            <h3>Jaco Terblanche Designs</h3>
+                            <p className="small-project-text">Website Design | Wordpress | MonthToMonth</p>
+                        </div>
+                        <div className="portfolio-column-div1">
+                            <img src={huweliksfokus} alt="Some of our latest website design projects" />
+                            <h3>Huweliksfokus Councillors</h3>
+                            <p className="small-project-text">Brand Development | Website Design | Wordpress</p>
+                        </div>
                     </div>
                     <div className="portfolio-column">
-                        <h2>Website designs our company just launched</h2>
-                        <h2>Website designs our company just launched</h2>
-                        <h2>Website designs our company just launched</h2>
-                        <h2>Website designs our company just launched</h2>
-                        <h2>Website designs our company just launched</h2>
+                        <div className="portfolio-column-div2">
+                            <img src={galaxydoors} alt="Some of our latest website design projects" />
+                            <h3>Galaxy Doors</h3>
+                            <p className="small-project-text">Brand Development | Website Design | SEO</p>
+                        </div>
+                        <div className="portfolio-column-div2">
+                            <img src={cycad} alt="Some of our latest website design projects" />
+                            <h3>Cycad Estate Polokwane</h3>
+                            <p className="small-project-text">Website Design | Wordpress</p>
+                        </div>
+                        <div className="portfolio-column-div2">
+                            <img src={greenco} alt="Some of our latest website design projects" />
+                            <h3>Tychoset Engineering</h3>
+                            <p className="small-project-text">Website Design | Wordpress</p>
+                        </div>
                     </div>
                 </div>
             </div>
