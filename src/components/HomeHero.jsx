2da90b6923411cf5cef1eee44d2164cc9a5d9528
Ian Cahn <iancahn@live.com>
2022-10-02 16:54:04 +0200

    PBP
    New style and font update

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -7,11 +7,11 @@ function HomeHero() {
             <video src={videoBg} autoPlay loop muted />
             <div className="hero-text">
                 <div className="hero-text-wrap">
-                    <h1 className="home-h1">Your online partner for innovative and impactful digital solutions.</h1>
-                    <div>
+                    <h1 className="home-h1">BIG IDEAS, <br />CREATIVE PEOPLE,<br /> NEW TECHNOLOGY.</h1>
+                    {/* <div>
                         <button className="black-btn">VIEW CASE STUDIES</button>
                         <button className="white-btn">CONTACT US NOW</button>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
--- src/components/HomeIntro.jsx
+++ src/components/HomeIntro.jsx
@@ -11,9 +11,15 @@ function HomeIntro() {
         <section>
             <div className="container">
                 <div className="row">
-                    <div className="column">
+                    {/* <div className="column">
                         <h2>How we stand out</h2>
                         <p className="large-text">By always improving our skillset, we continue to deliver innovative web solutions that drives real online success.</p>
+                        <br />
+                        <p>We believe great website design consists of more than just adding some images and words to a webpage. It's getting the customer to trust your company. It's getting visitors to convert into actual paying customers.</p>
+                    </div> */}
+                    <div className="column">
+                        <h2>Generate Leads and Grow Online</h2>
+                        <p className="large-text">We are web designers, developers, project managers, and digital marketing professionals dedicated to creative and results-driven web-based solutions using the latest trends and technologies.</p>
                         <br />
                         <p>We believe great website design consists of more than just adding some images and words to a webpage. It's getting the customer to trust your company. It's getting visitors to convert into actual paying customers.</p>
                     </div>
